b5ad5cc3cba73c8f72683e5497e4e346093d7f5d
robleda <diplastic@gmail.com>
2019-12-25 18:30:42 +0300

    Stairway done!

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,6 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
 	:param stairway: list of ints, where each int is a cost of appropriate step
 	:return: minimal cost of getting to the top
 	"""
-	print(stairway)
-	return 0
+	for i in range(2, len(stairway)):
+		stairway[i] += min(stairway[i - 1], stairway[i - 2])
+	return stairway[-1]
