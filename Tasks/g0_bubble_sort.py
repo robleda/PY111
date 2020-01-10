28a71823ad355160a4b5225a7029b3b81f2658dc
robleda <diplastic@gmail.com>
2020-01-10 21:18:06 +0300

    bubble

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -10,4 +10,16 @@ def sort(container: Collection[_Tt]) -> Collection[_Tt]:
 	:param container: container of elements to be sorted
 	:return: container sorted in ascending order
 	"""
+
+	for j in range(len(container)):
+		swapped = False
+		for i in range(len(container) - 1 - j):
+			if container[i] > container[i + 1]:
+				container[i], container[i + 1] = container[i + 1], container[i]
+				swapped = True
+
+		if not swapped:
+			break
+
+
 	return container
